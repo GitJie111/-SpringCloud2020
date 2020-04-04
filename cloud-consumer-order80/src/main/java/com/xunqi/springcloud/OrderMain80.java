38580f2866d2d12421ba3ec452592ec9956794e6
HeJieLin <1468041654@qq.com>
2020-04-04 17:55:03 +0800

    订单微服务80驻入EurekaServer

--- cloud-consumer-order80/src/main/java/com/xunqi/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/xunqi/springcloud/OrderMain80.java
@@ -7,8 +7,10 @@ package com.xunqi.springcloud;
  **/
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
 
     public static void main(String[] args) {
