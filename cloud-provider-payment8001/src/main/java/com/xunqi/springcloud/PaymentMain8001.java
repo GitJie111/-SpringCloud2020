713e217f5f654f8a7ca44b552870c045e846b02b
HeJieLin <1468041654@qq.com>
2020-04-04 17:43:43 +0800

    支付服务8001驻入EurekaServer

--- cloud-provider-payment8001/src/main/java/com/xunqi/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/xunqi/springcloud/PaymentMain8001.java
@@ -2,6 +2,7 @@ package com.xunqi.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Created with IntelliJ IDEA.
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @create: 2020-04-03 20:52
  **/
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
 
     public static void main(String[] args) {
